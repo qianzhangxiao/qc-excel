483efa518fad84fc9c74265a944120edd7eff4d3
qianzhangxiao <qianzhangxiao@163.com>
2023-03-01 16:59:03 +0800

    解决下拉菜单超过255字节限制

--- qcexcel-spring-boot-auto-configuration/src/main/java/com/qzx/excel/utils/SelfWriteHandle.java
+++ qcexcel-spring-boot-auto-configuration/src/main/java/com/qzx/excel/utils/SelfWriteHandle.java
@@ -3,14 +3,13 @@ package com.qzx.excel.utils;
 import com.alibaba.excel.write.handler.SheetWriteHandler;
 import com.alibaba.excel.write.metadata.holder.WriteSheetHolder;
 import com.alibaba.excel.write.metadata.holder.WriteWorkbookHolder;
-import org.apache.poi.ss.usermodel.DataValidation;
-import org.apache.poi.ss.usermodel.DataValidationConstraint;
-import org.apache.poi.ss.usermodel.DataValidationHelper;
-import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddressList;
-import org.apache.poi.xssf.usermodel.XSSFDataValidation;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SelfWriteHandle implements SheetWriteHandler {
 
@@ -29,18 +28,32 @@ public class SelfWriteHandle implements SheetWriteHandler {
      * */
     @Override
     public void afterSheetCreate(WriteWorkbookHolder writeWorkbookHolder, WriteSheetHolder writeSheetHolder) {
+        final Workbook workbook = writeWorkbookHolder.getWorkbook();
         Sheet sheet = writeSheetHolder.getSheet();
+        // 开始设置下拉框
         DataValidationHelper helper = sheet.getDataValidationHelper();
         for (Map.Entry<Integer, String[]> entry : mapDropDown.entrySet()) {
-            CellRangeAddressList addressList = new CellRangeAddressList(1, 1000, entry.getKey(), entry.getKey());
-            DataValidationConstraint constraint = helper.createExplicitListConstraint(entry.getValue());
-            DataValidation dataValidation = helper.createValidation(constraint, addressList);
-            if (dataValidation instanceof XSSFDataValidation) {
-                dataValidation.setSuppressDropDownArrow(true);
-                dataValidation.setShowErrorBox(true);
-            } else {
-                dataValidation.setSuppressDropDownArrow(false);
+            // 定义sheet的名称
+            String sheetName = "SheetHidden" + entry.getKey();
+            // 1.创建一个隐藏的sheet 名称为 privateSheet
+            Sheet privateSheet = workbook.createSheet(sheetName);
+            // 设置隐藏
+            workbook.setSheetHidden(workbook.getSheetIndex(sheetName), true);
+            // 2.循环赋值（为了防止下拉框的行数与隐藏域的行数相对应，将隐藏域加到结束行之后）
+            // 设置下拉框数据
+            List<String> values = Arrays.stream(entry.getValue()).collect(Collectors.toList());
+            for (int i = 0, length = values.size(); i < length; i++) {
+                // i:表示你开始的行数 0表示你开始的列数
+                privateSheet.createRow(i).createCell(0).setCellValue(values.get(i));
             }
+            Name category1Name = workbook.createName();
+            category1Name.setNameName(sheetName);
+            // 4 $A$1:$A$N代表 以A列1行开始获取N行下拉数据
+            category1Name.setRefersToFormula(sheetName + "!$A$1:$A$" + (values.size()));
+            // 5 将刚才设置的sheet引用到你的下拉列表中 //起始行、终止行、起始列、终止列
+            CellRangeAddressList addressList = new CellRangeAddressList(1, 65535, entry.getKey(), entry.getKey());
+            DataValidationConstraint constraint = helper.createFormulaListConstraint(sheetName);
+            DataValidation dataValidation = helper.createValidation(constraint, addressList);
             sheet.addValidationData(dataValidation);
         }
     }
